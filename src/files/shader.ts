fc22c7b10953fd80d05948a81a6ea9374bc5bf9f
Zoe <git@zoe.pw>
2024-10-02 12:40:04 -0300

    Added improvements to shader

--- src/files/shader.ts
+++ src/files/shader.ts
@@ -7,6 +7,38 @@ uniform bool iLightMode;
 uniform bool iDarkMode;
 
 
+float iAnimationSpeed = 0.15;
+float iAnimationZSpeed = 0.3;
+// Coefficient for the amount of color used.
+// For example, iColorAmount = 3.0, 1/3 of colors
+// will be used.
+float iColorAmount = 3.0;
+// Speed of color cycling
+float iColorSpeed = 7.0;
+
+//
+// Description : Array and textureless GLSL 2D simplex noise function.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+//
+
+//
+// Description : Array and textureless GLSL 2D/3D/4D simplex
+//               noise functions.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20201014 (stegu)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+//
+
 vec3 mod289(vec3 x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
@@ -25,7 +57,7 @@ vec4 taylorInvSqrt(vec4 r)
 }
 
 float snoise(vec3 v)
-  { 
+  {
   const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
   const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
 
@@ -48,10 +80,10 @@ float snoise(vec3 v)
   vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
 
 // Permutations
-  i = mod289(i); 
-  vec4 p = permute( permute( permute( 
+  i = mod289(i);
+  vec4 p = permute( permute( permute(
              i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
-           + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
+           + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
            + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
 
 // Gradients: 7x7 points over a square, mapped onto an octahedron.
@@ -95,12 +127,28 @@ float snoise(vec3 v)
 // Mix final noise value
   vec4 m = max(0.5 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
   m = m * m;
-  return 105.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
+  return 105.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
                                 dot(p2,x2), dot(p3,x3) ) );
   }
 
-float color(vec2 xy) { return 0.7 * snoise(vec3(xy, 0.3*iTime)); }
+float color(vec2 xy) { return 0.7 * snoise(vec3(xy, iAnimationZSpeed*iTime)); }
 
+// https://github.com/Experience-Monks/glsl-hsl2rgb
+// License: MIT
+// The MIT License (MIT) Copyright (c) 2015 Jam3
+// is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies
+// or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+// THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 float hue2rgb(float f1, float f2, float hue) {
     if (hue < 0.0)
@@ -119,38 +167,71 @@ float hue2rgb(float f1, float f2, float hue) {
     return res;
 }
 
+// https://github.com/Experience-Monks/glsl-hsl2rgb
+// License: MIT
+// The MIT License (MIT) Copyright (c) 2015 Jam3
+// is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies
+// or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+// THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 vec3 hsl2rgb(vec3 hsl) {
     vec3 rgb;
-    
+
     if (hsl.y == 0.0) {
         rgb = vec3(hsl.z); // Luminance
     } else {
         float f2;
-        
+
         if (hsl.z < 0.5)
             f2 = hsl.z * (1.0 + hsl.y);
         else
             f2 = hsl.z + hsl.y - hsl.y * hsl.z;
-            
+
         float f1 = 2.0 * hsl.z - f2;
-        
+
         rgb.r = hue2rgb(f1, f2, hsl.x + (1.0/3.0));
         rgb.g = hue2rgb(f1, f2, hsl.x);
         rgb.b = hue2rgb(f1, f2, hsl.x - (1.0/3.0));
-    }   
+    }
     return rgb;
 }
 
+// License: MIT
+// The MIT License (MIT) Copyright (c) 2015 zoepw
+// is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies
+// or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+// THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    vec2 relative = fragCoord.xy + vec2(0, iResolution.y * -iScrolled);
+    vec2 relative = fragCoord.xy + vec2(0, iResolution.y * - iScrolled);
 
     vec2 p = (relative/iResolution.y) * 2.0 - 1.0;
 
     vec3 xyz = vec3(p, 0);
 
-    vec2 relativePos = ((xyz.xy/2.0) + vec2(iTime/32.0, 0));
+    vec2 relativePos = ((xyz.xy/2.0) + vec2(iTime*iAnimationSpeed, 0));
 
-    float n = 0.5 + (0.5 * color(relativePos));
+    float n = (color(relativePos) + (sin(iTime/iColorSpeed)*(iColorAmount-1.0)))/iColorAmount;
 
     float lightness = .125;
 
@@ -160,14 +241,13 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
         saturation = .5;
     }
 
-    vec3 color = (hsl2rgb(vec3((n*360.0)/360.0, saturation, lightness)));
-        
+    vec3 color = hsl2rgb(vec3((n*360.0)/360.0, saturation, lightness));
+
     if (iLightMode) {
         color = vec3(1, 1, 1) - color;
     }
-    
-    fragColor.xyz = color;
 
+    fragColor.xyz = color;
 }
 
 void main() {
